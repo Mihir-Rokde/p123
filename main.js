8ccb59e327a9c43d7db4639a945a524884ac0ef5
Mihir-Rokde <100846891+Mihir-Rokde@users.noreply.github.com>
2023-01-27 19:32:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference=0
+leftWristX=0
+rightWristX=0
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -9,12 +13,21 @@ function setup(){
     function modelLoaded(){
         console.log("poseNet is intialized");
     }
-    function draw(){
-        background("grey");
-    }
+   
     function gotPoses(results){
     if(results.length>0)
     {
         console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=leftWristX-rightWristX;
+        console.log("leftWristX="+leftWristX+',rightWristX='+rightWristX+'difference='+difference);
     }
+    }
+    function draw(){
+        background("grey");
+        document.getElementById("font_size").innerHTML="font size of the text will be ="+difference+"px";
+        textSize(difference);
+        fill("yellow");
+        text("Mihir",50,400);
     }
